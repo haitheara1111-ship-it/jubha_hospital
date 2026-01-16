d2aaadf943964cbf1c3902684761dc35fd768dbb
DaraCOOKED <dara.somnag020@gmail.com>
2026-01-16 09:27:15 +0700

    FIx the section page and complate the hero section

---
+++ wp-content/themes/my-theme/page-get-in-touch.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style/page-get-in-touch.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+
+
+<section class="page-hero">
+  <div class="page-hero-overlay"></div>
+
+  <div class="page-hero-content">
+    <h1>Find in Touch</h1>
+
+    <div class="breadcrumb">
+      <span class="home-icon">
+        <i class="fa-solid fa-house"></i>
+      </span>
+      <a href="#">Contact</a>
+      <span class="slash">/</span>
+      <span class="current">Find in Touch</span>
+    </div>
+  </div>
+</section>
+
+
+<?php get_footer(); ?>
