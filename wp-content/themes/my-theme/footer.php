0d37797253f9a5697382a6f2a81aa0e6d75881db
DaraCOOKED <dara.somnag020@gmail.com>
2026-01-19 10:23:17 +0700

    new code i have the fix the footer code.

--- wp-content/themes/my-theme/footer.php
+++ wp-content/themes/my-theme/footer.php
@@ -21,92 +21,98 @@
   </div>
 
   <div class="footer-grid">
+
+    <!-- Column 1 -->
     <div class="footer-col">
-      <h4>Links</h4>
+      <h4>Quick Access</h4>
       <ul>
         <li><a href="#">Home</a></li>
-        <li><a href="#">Download / Get</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">OS Documentation</a></li>
-        <li><a href="#">Tool Documentation</a></li>
-        <li><a href="#">System Status</a></li>
-        <li><a href="#">Archived Releases</a></li>
-        <li><a href="#">Partnerships</a></li>
+        <li><a href="#">Find a Doctor</a></li>
+        <li><a href="#">Book an Appointment</a></li>
+        <li><a href="#">Departments & Clinics</a></li>
+        <li><a href="#">Health Packages</a></li>
+        <li><a href="#">Patient Guide</a></li>
+        <li><a href="#">Visiting Hours</a></li>
+        <li><a href="#">Contact Us</a></li>
       </ul>
     </div>
 
+    <!-- Column 2 -->
     <div class="footer-col">
-      <h4>Platforms</h4>
+      <h4>Medical Services</h4>
       <ul>
-        <li>ARM (SBC)</li>
-        <li>NetHunter (Mobile)</li>
-        <li>Amazon AWS</li>
-        <li>Docker</li>
-        <li>Linode</li>
-        <li>Microsoft Azure</li>
-        <li>Microsoft Store (WSL)</li>
-        <li>Vagrant</li>
+        <li>Emergency Care (24/7)</li>
+        <li>Outpatient Clinics</li>
+        <li>Inpatient Services</li>
+        <li>Diagnostic & Imaging</li>
+        <li>Laboratory Services</li>
+        <li>Pharmacy</li>
+        <li>Home Healthcare</li>
+        <li>Telemedicine</li>
       </ul>
     </div>
 
+    <!-- Column 3 -->
     <div class="footer-col">
-      <h4>Development</h4>
+      <h4>Patient Care</h4>
       <ul>
-        <li>Bug Tracker</li>
-        <li>Continuous Integration</li>
-        <li>Network Mirror</li>
-        <li>Package Tracker</li>
-        <li>GitLab</li>
+        <li>Appointments & Scheduling</li>
+        <li>Medical Records</li>
+        <li>Insurance & Billing</li>
+        <li>Patient Rights</li>
+        <li>Complaints & Suggestions</li>
       </ul>
     </div>
 
+    <!-- Column 4 -->
     <div class="footer-col">
-      <h4>Community</h4>
+      <h4>Community & Support</h4>
       <ul>
-        <li>Discord</li>
-        <li>Support Forum</li>
-        <li>PeerTube</li>
+        <li>Health Awareness Programs</li>
+        <li>Patient Support Groups</li>
+        <li>Volunteer Programs</li>
       </ul>
     </div>
 
+    <!-- Column 5 -->
+    <div class="footer-col">
+      <h4>Connect With Us</h4>
+      <ul>
+        <li>Hospital News</li>
+        <li>Health Tips</li>
+        <li>Events & Campaigns</li>
+        <li>Careers</li>
+        <li>Media Center</li>
+        <li>Mobile Application</li>
+        <li>Newsletter</li>
+      </ul>
+    </div>
+
+    <!-- Column 6 -->
     <div class="footer-col">
       <h4>Follow Us</h4>
-      <ul>
-        <li>Bluesky</li>
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>Mastodon</li>
-        <li>X</li>
-        <li>Newsletter</li>
-        <li>RSS</li>
+      <ul class="social-icons">
+        <li><a href="#"><i class="fa-brands fa-bluesky"></i> Bluesky</a></li>
+        <li><a href="#"><i class="fa-brands fa-facebook"></i> Facebook</a></li>
+        <li><a href="#"><i class="fa-brands fa-instagram"></i> Instagram</a></li>
+        <li><a href="#"><i class="fa-brands fa-mastodon"></i> Mastodon</a></li>
+        <li><a href="#"><i class="fa-brands fa-x-twitter"></i> X</a></li>
+        <li><a href="#"><i class="fa-solid fa-envelope"></i> Newsletter</a></li>
+        <li><a href="#"><i class="fa-solid fa-rss"></i> RSS</a></li>
       </ul>
     </div>
 
-    <div class="footer-col">
-  <h4>Follow Us</h4>
-  <ul class="social-icons">
-    <li><a href="#"><i class="fa-brands fa-bluesky"></i> Bluesky</a></li>
-    <li><a href="#"><i class="fa-brands fa-facebook"></i> Facebook</a></li>
-    <li><a href="#"><i class="fa-brands fa-instagram"></i> Instagram</a></li>
-    <li><a href="#"><i class="fa-brands fa-mastodon"></i> Mastodon</a></li>
-    <li><a href="#"><i class="fa-brands fa-x-twitter"></i> X</a></li>
-    <li><a href="#"><i class="fa-solid fa-envelope"></i> Newsletter</a></li>
-    <li><a href="#"><i class="fa-solid fa-rss"></i> RSS</a></li>
-  </ul>
-</div>
-
+    <!-- Brand -->
     <div class="footer-brand">
-      <img src=<?php echo get_template_directory_uri(); ?>/images/logo-hospital.png alt="logo">
-      <p>© OffSec Services Limited<br>2025. All rights reserved.</p>
+      <img src="<?php echo get_template_directory_uri(); ?>/images/logo-hospital-rm-bg.png" alt="logo">
+      <p>© Jubha Hospital<br><?php echo date('Y'); ?>. All rights reserved.</p>
     </div>
+
   </div>
 
-
-
-
-  <?php get_footer(); ?>
-
 </footer>
+
+<?php get_footer(); ?>
 
 
 
